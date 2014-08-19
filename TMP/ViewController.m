f7e5682987c2120cc4200a0c24defeec079ef997
salomf <salomykola@gmail.com>
2014-08-19 16:53:07 +0300

    new branch commit

--- TMP/ViewController.m
+++ TMP/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
@@ -16,6 +17,26 @@
             
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    CKRecordID *mainRecordID = [[CKRecordID alloc] initWithRecordName: @"Main"];
+    
+    CKRecord *mainRecord = [[CKRecord alloc] initWithRecordType: @"SomeRecord" recordID: mainRecordID];
+    
+    mainRecord [@"qwerty"] = @"1";
+    //[mainRecord setObject: @"qwerty" forKey: @"1"];
+    
+    CKDatabase *publicDB = [[CKContainer defaultContainer] publicCloudDatabase];
+    
+    [publicDB saveRecord:mainRecord completionHandler:^(CKRecord *savedRec, NSError *error){
+        NSLog(@"%@",savedRec);
+    }];
+    
+    [publicDB fetchRecordWithID:mainRecordID completionHandler:^(CKRecord *record, NSError *error) {
+        NSLog(@"%@",record);
+    }];
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
